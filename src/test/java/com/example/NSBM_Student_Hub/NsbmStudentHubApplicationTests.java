ab2a543553fde47e3389034bccef31072d247b80
Kaarthiraghav <152093294+Kaarthiraghav@users.noreply.github.com>
2026-02-09 15:07:13 +0530

    Initial Commit

---
+++ src/test/java/com/example/NSBM_Student_Hub/NsbmStudentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NSBM_Student_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsbmStudentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
